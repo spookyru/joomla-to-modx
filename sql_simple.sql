0126778d28b20c4e93948415f89905668386003a
Stanislav <81306953+spookyru@users.noreply.github.com>
2026-06-04 21:52:36 +0300

    Simplify menu joins and add GROUP BY clause
    Refactor SQL query to simplify menu joins and add grouping.

--- sql_simple.sql
+++ sql_simple.sql
@@ -3,7 +3,7 @@ SELECT
     c.title AS content_title,
     c.alias AS content_alias,
     c.introtext,
-    c.fulltext,
+    c.`fulltext`,
     c.created,
     c.modified,
     c.hits,
@@ -13,19 +13,15 @@ SELECT
     c.catid AS category_id,
     cat.title AS category_title,
     cat.alias AS category_alias,
-    m_cat.path AS menu_cat_path,
-    m_cat.alias AS menu_cat_alias,
-    m_cat.title AS menu_cat_title,
-    m_cat.link AS menu_cat_link,
-    m_cat.published AS menu_cat_published,
-    m_article.path AS menu_article_path,
-    m_article.alias AS menu_article_alias,
-    m_article.title AS menu_article_title,
-    m_article.link AS menu_article_link,
-    m_article.published AS menu_article_published
+    m.path AS menu_path,
+    m.alias AS menu_alias,
+    m.title AS menu_title,
+    m.link AS menu_link
 FROM #__content AS c
 LEFT JOIN #__categories AS cat ON c.catid = cat.id
-LEFT JOIN #__menu AS m_cat ON m_cat.link LIKE CONCAT('%option=com_content&view=category&id=', CAST(c.catid AS CHAR))
-LEFT JOIN #__menu AS m_article ON m_article.link LIKE CONCAT('%option=com_content&view=article&id=', CAST(c.id AS CHAR))
+LEFT JOIN #__menu AS m 
+    ON m.link LIKE CONCAT('%option=com_content&view=category&id=', CAST(c.catid AS CHAR))
+    AND m.published = 1
 WHERE c.state = 1
+GROUP BY c.id
 ORDER BY c.created DESC;
